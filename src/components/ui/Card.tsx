94abb96be6b93644c2737ab8c7725097e8acc139
jabez-abija2399 <jabeznew2399@gmail.com>
2026-01-10 23:51:46 +0300

    description card added

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -53,4 +53,16 @@ const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HT
 
 CardTitle.displayName = "CardTitle"
 
-export {Card, CardContent, CardHeader, CardTitle}
+const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
+    ({className, ...props}, ref) => (
+        <p
+        ref={ref}
+        className={cn("text-sm text-muted-foreground", className)}
+        {...props}
+        />
+    )
+)
+
+CardDescription.displayName = "CardDescription"
+
+export {Card, CardContent, CardHeader, CardTitle, CardDescription}
