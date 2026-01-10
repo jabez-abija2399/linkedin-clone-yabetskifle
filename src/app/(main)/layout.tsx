1790a3432b8f4ce5dd49fc03da5a029224f0896d
jabez-abija2399 <jabeznew2399@gmail.com>
2026-01-10 22:04:11 +0300

    layout created

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "@/components/layout/Navbar";
+
+export default function MainLayout(
+    {children}: 
+    {children: React.ReactNode}) {
+    return (
+        <div className="min-h-screen flex flex-col bg-background">
+            <Navbar />
+            
+            <main className="flex-1 max-w-7xl mx-auto w-full px-4 py-6">
+                {children}
+            </main>
+        </div>
+    )
+}
