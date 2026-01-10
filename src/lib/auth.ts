7afcaa2c6b9b37f2df54de9ac4124e9c5ea857ec
jabez-abija2399 <jabeznew2399@gmail.com>
2026-01-10 23:34:48 +0300

    Github provider added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,12 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./prisma";
+import Github from "next-auth/providers/github";
 
 export const { handlers, auth, signIn, signOut, } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
+        Github,
         
     ]
 })
