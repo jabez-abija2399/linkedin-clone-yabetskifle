b02a7eeb559dfeded3bd1ab44dffacec3323b8b8
jabez-abija2399 <jabeznew2399@gmail.com>
2026-01-12 15:32:48 +0300

    comment action created to create comments

---
+++ src/server/actions/comment.actions.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+
+export async function CreateComment(content: string, postId: string) {
+    const userId = "cmka10j6l0000ux9l430r1vte"; // Use our seed user ID for now
+    
+    if(!content || !postId) {
+        return { error: "Invalid comment data" }
+    }
+
+    try {
+        await prisma.comment.create({
+            data: {
+                authorId: userId,
+                postId,
+                content,
+            }
+        })
+    } catch (error) {
+        console.error("Error creating comment:", error);
+        return { error: "Failed to create comment" };
+    }
+
+    revalidatePath("/")
+}
