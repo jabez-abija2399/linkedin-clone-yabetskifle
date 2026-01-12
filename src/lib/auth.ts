66c6eb4c926f198402b1307016410175cae4dc6d
jabez-abija2399 <jabeznew2399@gmail.com>
2026-01-12 20:25:35 +0300

    callback sesson is created

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,10 +3,15 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./prisma";
 import Github from "next-auth/providers/github";
 
-export const { handlers, auth, signIn, signOut, } = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        Github,
-        
-    ]
-})
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [Github],
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id; // 👈 This makes the ID available everywhere!
+      }
+      return session;
+    },
+  },
+});
