415c21e1f6a1e323beb747528ca47201113a5159
jabez-abija2399 <jabeznew2399@gmail.com>
2026-01-13 10:10:08 +0300

    hardecoded user changed to changed to session

--- src/server/actions/like.actions.ts
+++ src/server/actions/like.actions.ts
@@ -1,9 +1,15 @@
 "use server"
+import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
 export async function toggleLike(postId: string) {
-    const userId = "cmka10j6l0000ux9l430r1vte"; // Use our seed user ID for now
+    const session = await auth()
+    const userId = session?.user?.id
+
+    if (!userId) {
+        return { error: "Unauthorized" }
+    }
 
     const like = await prisma.like.findUnique({
         where: {
