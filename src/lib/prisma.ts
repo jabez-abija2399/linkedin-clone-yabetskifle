e43aa0696c874001b2f27d8a02b63a6b5a581629
jabez-abija2399 <jabeznew2399@gmail.com>
2026-01-14 23:58:35 +0300

    feat: Implement "People you may know" suggestions component and outline future milestones in the README.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,7 @@ const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
 };
 
-export const prisma =
+export const prisma: any =
     globalForPrisma.prisma ||
     new PrismaClient({
         log: ["query"],
