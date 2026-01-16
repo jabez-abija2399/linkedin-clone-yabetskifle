bac9f0eca4d6a17779d0f5650f8b762c750bce2e
jabez-abija2399 <jabeznew2399@gmail.com>
2026-01-16 13:58:04 +0300

    feat: implement initial messaging UI components and update README with messaging milestones.

---
+++ src/components/messaging/ConversationItem.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { format } from "date-fns";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+
+interface ConversationItemProps {
+    conversation: any;
+    currentUserId: string;
+    isActive: boolean;
+}
+
+export const ConversationItem = ({conversation, currentUserId, isActive}: ConversationItemProps) => {
+    const router = useRouter()
+
+    // find the otheruser
+    const otheruser = conversation.users.find((u:any) => u.id !== currentUserId);
+    const lastMessage = conversation.messages.at[0];
+    return (
+       <div>
+        <div>
+            <Image 
+               src={otheruser?.image }
+               alt={otheruser?.name}
+               fill
+               className="rounded-full object-cover"
+            />
+        </div>
+        <div>
+            <div>
+                <span>{otheruser?.name}</span>
+                <span>{lastMessage?.createdAt && format(new Date(lastMessage.createdAt), "MMM d")}</span>
+
+            </div>
+            <p>
+                {lastMessage?.senderId === currentUserId && "You: "}
+                {lastMessage?.content || "start a conversation"}
+            </p>
+        </div>
+       </div>
+    )
+}
